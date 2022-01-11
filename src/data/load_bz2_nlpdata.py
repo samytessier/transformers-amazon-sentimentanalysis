018b8b0976c47951e82325c1abd54b3191dc4ede
paulseghers <0099seghers@gmail.com>
2022-01-11 18:28:16 +0100

    new embedding method

--- src/data/load_bz2_nlpdata.py
+++ src/data/load_bz2_nlpdata.py
@@ -8,21 +8,9 @@ from torchtext.data import get_tokenizer
 from torchtext.legacy.data import Field
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
-from torchtext.vocab import FastText, voca
-bimport torch
+from torchtext.vocab import FastText, vocab
+import torch
 #from torchtext.vocab import GloVe
-
-"""
-file with methods to:
-    - import .bz2 datasets
-    - extract texts
-    - normalize texts
-    - tokenize, embed, create vectors shaped for CNN
-    - create DataLoaders
-    
-currently they are still barebones and some would benefit to be wrapped in functions
-"""
-
 
 ##############################
 #    read raw gros bizou2    #
@@ -60,6 +48,7 @@ def normalize_texts(texts):
         normalized_texts.append(no_non_ascii)
     return normalized_texts
 
+
 def amzreview_dataset() -> (DataLoader, DataLoader):
     """
     reads files from /data/raw with appropriate method 
@@ -72,7 +61,7 @@ def amzreview_dataset() -> (DataLoader, DataLoader):
 
     train_texts = normalize_texts(train_texts)
     test_texts = normalize_texts(test_texts)
-
+    #print("#txts mod 64: ", len(train_texts) % 64)
     #MAX_SEQ_SIZE = max([len(t) for t in train_texts+test_texts])
 
 
@@ -83,33 +72,40 @@ def amzreview_dataset() -> (DataLoader, DataLoader):
     train_texts_tok = [Tokenizer(t) for t in train_texts]
     test_texts_tok  = [Tokenizer(t) for t in test_texts]
     #we need to store the tokenized texts anyways 
-    text_field = Field(tokenize=None, lower=True)
     #tokenizer: string -> :list[:string] | splits on " " char 
-
 
     #####################
     #    build vocab    #
     #####################
-    text_field.build_vocab(train_texts_tok, 
-        vectors='fasttext.simple.300d')# get the vocab instance
-    vocab = text_field.vocab
-    #print("vocab[are]: ", vocab['stuning'], "len(vocab): ", len(vocab))
-    # load fasttext simple embedding with 300d
-    embedding = FastText('simple') #[CONFIG] following should be stored in a config file 
+    vecs = FastText('simple') #[CONFIG] following should be stored in a config file 
+    
+    voc = vocab({w:1 for w in vecs.itos})
+    unk_idx = len(voc)-1
+    voc.set_default_index(unk_idx)
+    vecs.vectors[unk_idx] = vecs.vectors.mean(0)
 
-    NUM_EMBEDDINGS = len(vocab)
-    LEN_EMBEDDINGS = len(embedding[0])
-    print("dim embeddings: {}\n num embeddings: {}".format(LEN_EMBEDDINGS, NUM_EMBEDDINGS))
+    embed = torch.nn.Embedding.from_pretrained(vecs.vectors)
 
+    """
+    brief explanation of what's going on for my friends:
+    - tokenizer: splits (cleaned/normalized) sentences into words
+    - vocab: replace words by a number
+      -> where do we get this number from? the fastText library, pre-trained
+    - unk is just the encoding for words we don't know, i.e. don't see early on
+    when building the vocab
+    - embedding turns these numbers into vectors
+      -> what number becomes what vector is also determined by fasttext
+    """
 
     #############################
     #    back to prepro loop    #
     #############################
 
     #print("traintexttok[0]: ", train_texts_tok[0], "\n len: ", len(train_texts_tok[0]))
+    #print("traintexttok0: ",train_texts_tok[0])
 
-    train_texts_i= [torch.tensor([vocab[i] for i in t]) for t in train_texts_tok]
-    test_texts_i = [torch.tensor([vocab[i] for i in t]) for t in test_texts_tok]   
+    train_texts_i= [embed(torch.tensor([voc[i] for i in t])) for t in train_texts_tok]
+    test_texts_i = [embed(torch.tensor([voc[i] for i in t])) for t in test_texts_tok]   
 
     train_texts_padded = pad_sequence(train_texts_i, batch_first=True)
     test_texts_padded  = pad_sequence(test_texts_i, batch_first=True)
@@ -117,12 +113,17 @@ def amzreview_dataset() -> (DataLoader, DataLoader):
     train_data= [ [text, train_labels[i]] for i,text in enumerate(train_texts_padded)]
     test_data = [ [text, test_labels[i]] for i,text in enumerate(test_texts_padded)]
 
+    batch_size=64 # (CONFIG FILE !!)
+
+    cutoff_test = len(train_texts_i) % batch_size
+    cutoff_train= len(test_texts_i) % batch_size
     ###################
     #   dataloader    #
     ###################
 
-    train = DataLoader(train_data, shuffle=True, batch_size=64)
-    test = DataLoader(test_data, shuffle=True, batch_size=64)
+    train = DataLoader(train_data[:-cutoff_train], shuffle=True, batch_size=batch_size)
+    test = DataLoader(test_data[:-cutoff_test], shuffle=True, batch_size=batch_size)
     return train, test
 
 #amzreview_dataset()
+
